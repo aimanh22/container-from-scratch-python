8d7166ce97fe22be45048740c24fde5de4dbacfd
Aiman <69780552+aimanh22@users.noreply.github.com>
2020-10-16 12:30:52 +0530

    trial 1

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import click
+import sys
+import spacy
 
 @click.command()
 @click.option("--name")
@@ -8,4 +10,10 @@ def hello(name):
 
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
+    input = sys.argv[1:] 
+    str = "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
+    nlp = spacy.load('en')
+    doc=nlp(str)
+    for ent in doc.ents:
+        print(ent.text, ent.label_)
     hello()
